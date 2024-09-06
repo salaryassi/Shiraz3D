c894f22105551ade8bd303c01040094c29dd09e7
salaryassi <mst.magpi@gmail.com>
2024-09-06 20:54:37 +0330

    run

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -1,14 +1,17 @@
-const { merge } = require('webpack-merge')
-const commonConfiguration = require('./webpack.common.js')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require('path');
+const { merge } = require('webpack-merge');
+const commonConfiguration = require('./webpack.common.js');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = merge(
-    commonConfiguration,
-    {
-        mode: 'production',
-        plugins:
-        [
-            new CleanWebpackPlugin()
-        ]
-    }
-)
+module.exports = merge(commonConfiguration, {
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, '../build'), // Ensure the build directory is correct
+    filename: 'bundle.[contenthash].js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    // Add other plugins if needed
+  ],
+  // Add other configurations if needed
+});
